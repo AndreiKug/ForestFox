f4e995089dd457c8bc1d6d977d423cc197ca7766
AndreiKug <anarki3@yandex.ru>
2022-06-10 22:02:58 +0300

    Added demo sound for Eng numbers on trigger enter in Letters.cs. Changed quality of all music tracks.

--- Assets/Scripts/Logic/Letters.cs
+++ Assets/Scripts/Logic/Letters.cs
@@ -6,19 +6,33 @@ public class Letters : MonoBehaviour
 {
     private SpawnAndLoad _mySpawn;
 
+    private AudioSource _sound;
+    private BoxCollider _collider;
+    private MeshRenderer _mesh;
+
     private void Start()
     {
         GameObject spawnClass = GameObject.Find("ScriptHandler");
         _mySpawn = spawnClass.GetComponent<SpawnAndLoad>();
+
+        _sound = gameObject.GetComponent<AudioSource>();
+        _collider = gameObject.GetComponent<BoxCollider>();
+        _mesh = gameObject.GetComponent<MeshRenderer>();
+
     }
 
+    //Т.к. удаленный объект не может воспроизвести звук, сначала скрываем и воспроизводим звук, и через 1f sec удаляем
     public void OnTriggerEnter(Collider other)
     {
-       if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            _sound.Play();
+            _collider.enabled = false;
+            _mesh.enabled = false;
+            Destroy(gameObject, 1f);
 
-            Destroy(gameObject);
             _mySpawn.SpawnLetters();
+
         }
     }
 }
